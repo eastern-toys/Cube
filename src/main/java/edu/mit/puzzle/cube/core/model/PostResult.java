c084058a40b0d8bcaf8155bdadd3b6e680b6d636
Matt Gruskin <matthew.gruskin@gmail.com>
2016-06-02 22:45:53 -0400

    Replace manually built created/processed/updated responses with a PostResult class.

---
+++ src/main/java/edu/mit/puzzle/cube/core/model/PostResult.java
@@ -0,0 +1,25 @@
+package edu.mit.puzzle.cube.core.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+
+import javax.annotation.Nullable;
+
+@AutoValue
+public abstract class PostResult {
+    @AutoValue.Builder
+    public static abstract class Builder {
+        @Nullable @JsonProperty("created") public abstract Builder setCreated(Boolean created);
+        @Nullable @JsonProperty("processed") public abstract Builder setProcessed(Boolean processed);
+        @Nullable @JsonProperty("updated") public abstract Builder setUpdated(Boolean updated);
+        public abstract PostResult build();
+    }
+
+    public static Builder builder() {
+        return new AutoValue_PostResult.Builder();
+    }
+
+    @Nullable @JsonProperty("created") public abstract Boolean getCreated();
+    @Nullable @JsonProperty("processed") public abstract Boolean getProcessed();
+    @Nullable @JsonProperty("updated") public abstract Boolean getUpdated();
+}
--- src/main/java/edu/mit/puzzle/cube/core/serverresources/EventsResource.java
+++ src/main/java/edu/mit/puzzle/cube/core/serverresources/EventsResource.java
@@ -1,26 +1,27 @@
 package edu.mit.puzzle.cube.core.serverresources;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.google.common.collect.ImmutableMap;
+
 import edu.mit.puzzle.cube.core.events.Event;
+import edu.mit.puzzle.cube.core.model.PostResult;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.restlet.ext.json.JsonRepresentation;
-import org.restlet.representation.Representation;
 import org.restlet.resource.Post;
+
 import java.io.IOException;
 
 public class EventsResource extends AbstractCubeResource {
 
     @Post
-    public Representation handlePost(JsonRepresentation representation) throws JsonProcessingException {
+    public PostResult handlePost(JsonRepresentation representation) throws JsonProcessingException {
         try {
             JSONObject obj = representation.getJsonObject();
             Event event = eventFactory.generate(obj.toString());
 
             eventProcessor.process(event);
-            return new JsonRepresentation(MAPPER.writeValueAsString(ImmutableMap.of("processed",true)));
-
+            return PostResult.builder().setProcessed(true).build();
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (JSONException e) {
